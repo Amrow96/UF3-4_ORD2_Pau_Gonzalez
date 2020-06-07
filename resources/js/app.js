f8b7943e12c8d3bb822b16b3493cb411d6f78e8c
Pau Gonzàlez Martí <lazytime96@gmail.com>
2020-06-07 10:58:37 +0200

    Configurat VUE

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,12 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+import Vue from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +22,13 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component("usuaris", require("./components/Usuaris.vue").default);
+Vue.component("cursos", require("./components/Cursos.vue").default);
+Vue.use(BootstrapVue);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +37,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app"
 });
